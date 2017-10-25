1f1a9acd6d7cf4b3bc047e26811683c3cbf6eb42
Christophe De La Fuente <paristvinternet-github@yahoo.com>
2017-10-25 11:15:48 -0300

    Improve File#rename, File#delete, SetInfoRequest and the related File Information Classes

--- lib/ruby_smb/fscc/file_information.rb
+++ lib/ruby_smb/fscc/file_information.rb
@@ -1,7 +1,7 @@
 module RubySMB
   module Fscc
     # Contains the Constant values for File Information Classes, as defined in
-    # [2.2.33 SMB2 QUERY_DIRECTORY Request](https://msdn.microsoft.com/en-us/library/cc246551.aspx)
+    # [2.4 File Information Classes](https://msdn.microsoft.com/en-us/library/cc232064.aspx)
     module FileInformation
       require 'ruby_smb/fscc/file_information/file_directory_information'
       require 'ruby_smb/fscc/file_information/file_full_directory_information'
@@ -11,6 +11,16 @@ module RubySMB
       require 'ruby_smb/fscc/file_information/file_id_both_directory_information'
       require 'ruby_smb/fscc/file_information/file_names_information'
       require 'ruby_smb/fscc/file_information/file_rename_information'
+
+      FILE_DIRECTORY_INFORMATION         = 0x01
+      FILE_FULL_DIRECTORY_INFORMATION    = 0x02
+      FILE_DISPOSITION_INFORMATION       = 0x0D
+      FILE_ID_FULL_DIRECTORY_INFORMATION = 0x26
+      FILE_BOTH_DIRECTORY_INFORMATION    = 0x03
+      FILE_ID_BOTH_DIRECTORY_INFORMATION = 0x25
+      FILE_NAMES_INFORMATION             = 0x0C
+      FILE_RENAME_INFORMATION            = 0x0A
+
     end
   end
 end
--- lib/ruby_smb/smb2/packet/set_info_request.rb
+++ lib/ruby_smb/smb2/packet/set_info_request.rb
@@ -6,16 +6,45 @@ module RubySMB
       class SetInfoRequest < RubySMB::GenericPacket
         endian :little
 
-        smb2_header           :smb2_header
-        uint16                :structure_size,         label: 'Structure Size',         initial_value: 33
-        uint8                 :info_type,              label: 'Info Type',              initial_value: 0x01
-        uint8                 :file_info_class,        label: 'File Info Class'
-        uint32                :buffer_length,          label: 'Buffer Length'
-        uint16                :buffer_offset,          label: 'Buffer Offset',          initial_value: 96
-        uint16                :reserved,               label: 'Reserved',               initial_value: 0
-        uint32                :additional_information, label: 'Additional Information', initial_value: 0
-        smb2_fileid           :file_id,                label: 'File ID'
-        string                :buffer,                 label: 'Buffer'
+        smb2_header :smb2_header
+        uint16      :structure_size,  label: 'Structure Size', initial_value: 33
+        # info_type constants?
+        uint8       :info_type,       label: 'Info Type',      initial_value: 0x01
+        uint8       :file_info_class, label: 'File Info Class'
+        uint32      :buffer_length,   label: 'Buffer Length',  initial_value: -> { buffer.do_num_bytes }
+        uint16      :buffer_offset,   label: 'Buffer Offset',  initial_value: -> { buffer.abs_offset }
+        uint16      :reserved,        label: 'Reserved',       initial_value: 0
+
+        struct :additional_information do
+          bit1  :reserved,                       label: 'Reserved Space'
+          bit1  :scope_security_information,     label: 'Scope Security Information'
+          bit1  :attribute_security_information, label: 'Attribute Security Information'
+          bit1  :label_security_information,     label: 'Label Security Information'
+          bit1  :sacl_security_information,      label: 'SACL Security Information'
+          bit1  :dacl_security_information,      label: 'DACL Security Information'
+          bit1  :group_security_information,     label: 'Group Security Information'
+          bit1  :owner_security_information,     label: 'Owner Security Information'
+          # byte boundary
+          bit8  :reserved2,                      label: 'Reserved Space'
+          # byte boundary
+          bit7  :reserved3,                      label: 'Reserved Space'
+          bit1  :backup_security_information,    label: 'Backup Security Information'
+          # byte boundary
+          bit8  :reserved4,                      label: 'Reserved Space'
+        end
+
+        smb2_fileid :file_id, label: 'File ID'
+
+        choice :buffer, label: 'Buffer', selection: -> { file_info_class } do
+          file_disposition_information       RubySMB::Fscc::FileInformation::FILE_DIRECTORY_INFORMATION,         label: 'File Directory Information'
+          file_full_directory_information    RubySMB::Fscc::FileInformation::FILE_FULL_DIRECTORY_INFORMATION,    label: 'File Full Directory Information'
+          file_disposition_information       RubySMB::Fscc::FileInformation::FILE_DISPOSITION_INFORMATION,       label: 'File Disposition Information'
+          file_id_full_directory_information RubySMB::Fscc::FileInformation::FILE_ID_FULL_DIRECTORY_INFORMATION, label: 'File Id Full Directory Information'
+          file_both_directory_information    RubySMB::Fscc::FileInformation::FILE_BOTH_DIRECTORY_INFORMATION,    label: 'File Both Directory Information'
+          file_id_both_directory_information RubySMB::Fscc::FileInformation::FILE_ID_BOTH_DIRECTORY_INFORMATION, label: 'File Id Both Directory Information'
+          file_names_information             RubySMB::Fscc::FileInformation::FILE_NAMES_INFORMATION,             label: 'File Names Information'
+          file_rename_information            RubySMB::Fscc::FileInformation::FILE_RENAME_INFORMATION,            label: 'File Rename Information'
+        end
 
         def initialize_instance
           super
